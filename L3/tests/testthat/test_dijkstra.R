09372e9e4d2793f3231ee2e70dff3462e91a5550
krzbar <bartoszekkj@gmail.com>
2017-08-24 21:04:03 +0200

    wiki

--- L3/tests/testthat/test_dijkstra.R
+++ L3/tests/testthat/test_dijkstra.R
@@ -15,8 +15,8 @@ test_that("Error messages are returned for erronous input in the Dijkstra algori
   wiki_wrong_graph <- wiki_graph
   names(wiki_wrong_graph) <- c("v1, v3, w")
   expect_error(dijkstra(wiki_wrong_graph, 3))
-  wiki_wrong_graph <- wiki_graph[1:2]
-  expect_error(dijkstra(wiki_wrong_graph, 3))
+  wiki_wrong_graph2 <- wiki_graph[1:2]
+  expect_error(dijkstra(wiki_wrong_graph2, 3))
   expect_error(dijkstra(wiki_graph, 7))
   expect_error(dijkstra(as.matrix(wiki_graph), 3))  
 })
